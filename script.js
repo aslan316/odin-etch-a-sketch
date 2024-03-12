dfdcb226524f5dad4cf8d64c4156e765adccdb3e
aslan316 <ljhord31@gmail.com>
2024-03-12 08:40:32 -0400

    Fix grid color changing and add comments for readability

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector("#grid");
+//so that js can reference these values in makeGrid() without the css being loaded
 grid.style.width = "560px";
 grid.style.height = "560px";
 const scaleButton = document.querySelector("#scale-button");
@@ -10,11 +11,14 @@ greyScaleButton.disabled = true;
 makeGrid(16);
 
 grid.addEventListener("mouseover", (e) => {
-    if (greyScaleButton.disabled == true) {
-        e.target.style.backgroundColor = "black";
-    } else if (rainbowButton.disabled == true) {
-        e.target.style.backgroundColor = "rgb(" + randomColorValue() + "," + randomColorValue() + "," + randomColorValue() + ")";
+    if (e.target != grid) { //to mitigate changing the color of the grid if there is a small gap
+        if (greyScaleButton.disabled == true) {
+            e.target.style.backgroundColor = "black";
+        } else if (rainbowButton.disabled == true) {
+            e.target.style.backgroundColor = "rgb(" + randomColorValue() + "," + randomColorValue() + "," + randomColorValue() + ")";
+        }
     }
+    
 });
 
 function randomColorValue() {
@@ -22,10 +26,10 @@ function randomColorValue() {
 }
 
 scaleButton.addEventListener("click", () => {
-    let numCellsPerRow = Number(prompt("How many cells per row? (1-100)"));
+    let numCellsPerRow = Number(prompt("How many cells per row? (1-100)", "16"));
 
     while (numCellsPerRow > 100) {
-        numCellsPerRow = Number(prompt("How many cells per row? (1-100)"));
+        numCellsPerRow = Number(prompt("How many cells per row? (1-100)", "16"));
     }
 
     makeGrid(numCellsPerRow);
@@ -44,6 +48,7 @@ greyScaleButton.addEventListener("click", () => {
     greyScaleButton.disabled = true;
     rainbowButton.disabled = false;
 
+    //creates the "toggle" effect for the buttons
     greyScaleButton.style.color = "rgb(34,49,29)";
     greyScaleButton.style.backgroundColor = "rgb(159, 209, 129)";
     greyScaleButton.style.border = "4px solid rgb(34,49,29)";
@@ -57,6 +62,7 @@ rainbowButton.addEventListener("click", () => {
     rainbowButton.disabled = true;
     greyScaleButton.disabled = false;
 
+    //creates the "toggle" effect for the buttons
     rainbowButton.style.color = "rgb(34,49,29)";
     rainbowButton.style.backgroundColor = "rgb(159, 209, 129)";
     rainbowButton.style.border = "4px solid rgb(34,49,29)";
